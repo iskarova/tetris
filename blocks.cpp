ac82f0e7236cce227741eed1994d927c154d3af5
lina iskarova <cyberdeadushka@gmail.com>
2025-04-19 18:49:33 +0300

    create the blocks

---
+++ blocks.cpp
@@ -0,0 +1,11 @@
+#include "block.h"
+#include "position.h"
+
+class LBlcok : public Block{
+
+public: 
+	LBlock() {
+		id = 1;
+		cells[0] = { Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2) };
+	}
+};
