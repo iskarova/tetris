0ab2f1ad197e0747cd6c7ebb5fd2d918384875a9
lina iskarova <cyberdeadushka@gmail.com>
2025-04-24 00:02:25 +0300

    create UI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+﻿#include <iostream>
 #include "raylib.h"
 #include "game.h"
+#include "colors.h"
 
 double lastUpdateTime = 0;
 bool eventTriggered(double interval);
@@ -8,19 +9,28 @@ bool eventTriggered(double interval);
 using namespace std;
 int main() {
 
-	Color darkBlue = { 44, 44, 127, 255 };
-	InitWindow(300, 600, "tetris");
+	InitWindow(540, 620, "tetris");
 	SetTargetFPS(60);
+
+	Font font = LoadFontEx("font/monogram.ttf", 64, 0, 0);
 
 	Game game = Game();
 
 	while (!WindowShouldClose()) {
 		game.handleInput();
-		if (eventTriggered(0.05)) {
+		if (eventTriggered(0.5)) {
 			game.moveBlockDown();
 		}
 		BeginDrawing();
 		ClearBackground(darkBlue);
+		DrawTextEx(font, "SCORE", { 375, 15 }, 38, 1.5, WHITE);
+		DrawTextEx(font, "NEXT", { 385, 175 }, 38, 1.5, WHITE);
+		
+		if (game.gameOver)
+			DrawTextEx(font, "GAME OVER!", { 335, 450 }, 38, 1.5, MAGENTA);
+		
+		DrawRectangleRounded({ 340, 55, 170, 60 }, 0.3, 6, lightBlue);
+		DrawRectangleRounded({ 340, 215, 170, 180 }, 0.3, 6, lightBlue);
 		game.Draw();
 		EndDrawing();
 	}
